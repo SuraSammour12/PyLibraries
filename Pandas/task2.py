fc39feacca536e7105e9d94b1dd10b69ba78d22b
SuraSammour12 <surasammour2001@gmail.com>
2024-12-14 21:33:12 +0200

    Add files via upload

---
+++ Pandas/task2.py
@@ -0,0 +1,10 @@
+# Task :DataFrame
+import pandas as pd
+lst = [
+    {'Product':'Sweater','Price': 35,'Quantity': 20,'Discount': 5},
+    {'Product':'Jacket','Price': 60,'Quantity': 15,'Discount': 20},
+    {'Product':'Coffee Mug','Price': 10,'Quantity': 100,'Discount': 5},
+    {'Product':'Glass Cup','Price': 12,'Quantity': 60,'Discount': 15}
+]
+df=pd.DataFrame(lst)
+print(df)
