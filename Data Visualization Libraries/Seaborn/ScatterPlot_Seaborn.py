59cdf028b9a8aab843d96c50fee8c085a098c89b
SuraSammour12 <surasammour2001@gmail.com>
2024-12-23 02:30:05 +0200

    Add files via upload

--- Data Visualization Libraries/Seaborn/ScatterPlot_Seaborn.py
+++ Data Visualization Libraries/Seaborn/ScatterPlot_Seaborn.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Load the data from the CSV file into a DataFrame
+# Load the built-in 'tips' dataset from Seaborn
 df = sns.load_dataset('tips')
 
 # Set the size of the plot
